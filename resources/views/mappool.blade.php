f1ae216ea59b72e1de3f50d6c67374fc696a3e29
joji-fuck <logvmaks@gmail.com>
2025-06-19 15:08:05 +0500

    fix:mappool page

--- resources/views/mappool.blade.php
+++ resources/views/mappool.blade.php
@@ -1,106 +1,274 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Маппул</h1>
+    <h1 class="mappool-h1">Маппул</h1>
 
-    <details class="details details-blue">
+    <details class="details">
         <summary class="summary summary-blue">Nomod</summary>
-        <div class="map-card">
-            <div class="map-img">
-                <h2>Epitaph</h2>
-                <h3>Tears of tragedy</h3>
+        <a class="map-card map-card-blue" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
             </div>
             <div class="map-mode">
                 <p>NM1</p>
             </div>
             <div class="map-stats">
                 <div class="map-stats-left">
-                    <span>7,19</span>
-                    <span>191</span>
-                    <span>5:35</span>
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
                 </div>
                 <div class="map-stats-right">
-                    <span>4</span>
-                    <span>9,7</span>
-                    <span>9,7</span>
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
                 </div>
             </div>
-        </div>
-    </details>
-
-
-
-    <details>
-        <summary>DoubleTime</summary>
-        <div class="map-card">
-            <div class="map-img">
-                <h2>Epitaph</h2>
-                <h3>Tears of tragedy</h3>
-                <div class="map-mode">
-                    <span>NM1</span>
-                </div>
+        </a>
+        <a class="map-card map-card-blue" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>NM2</p>
             </div>
             <div class="map-stats">
                 <div class="map-stats-left">
-                    <span>7,19</span>
-                    <span>191</span>
-                    <span>5:35</span>
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
                 </div>
                 <div class="map-stats-right">
-                    <span>4</span>
-                    <span>9,7</span>
-                    <span>9,7</span>
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
                 </div>
             </div>
-        </div>
-    </details>
-    <details>
-        <summary>Hardrock</summary>
-        <div class="map-card">
-            <div class="map-img">
-                <h2>Epitaph</h2>
-                <h3>Tears of tragedy</h3>
-                <div class="map-mode">
-                    <span>NM1</span>
-                </div>
+        </a>
+        <a class="map-card map-card-blue" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>NM3</p>
             </div>
             <div class="map-stats">
                 <div class="map-stats-left">
-                    <span>7,19</span>
-                    <span>191</span>
-                    <span>5:35</span>
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
                 </div>
                 <div class="map-stats-right">
-                    <span>4</span>
-                    <span>9,7</span>
-                    <span>9,7</span>
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
                 </div>
             </div>
-        </div>
+        </a>
+
     </details>
-    <details>
-        <summary>Hidden</summary>
-        <div class="map-card">
-            <div class="map-img">
-                <h2>Epitaph</h2>
-                <h3>Tears of tragedy</h3>
-                <div class="map-mode">
-                    <span>NM1</span>
-                </div>
+
+    <details class="details">
+        <summary class="summary summary-red">DoubleTime</summary>
+        <a class="map-card map-card-red" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>DT1</p>
             </div>
             <div class="map-stats">
                 <div class="map-stats-left">
-                    <span>7,19</span>
-                    <span>191</span>
-                    <span>5:35</span>
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
                 </div>
                 <div class="map-stats-right">
-                    <span>4</span>
-                    <span>9,7</span>
-                    <span>9,7</span>
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </a>
+        <a class="map-card map-card-red" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>DT2</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </a>
+        <a class="map-card map-card-red" href="https://osu.ppy.sh/beatmapsets/2373050#osu/5123942">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>DT3</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </a>
+    </details>
+
+    <details class="details">
+        <summary class="summary summary-green">HardRock</summary>
+        <div class="map-card map-card-green">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HR1</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </div>
+        <div class="map-card map-card-green">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HR2</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </div>
+        <div class="map-card map-card-green">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HR3</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
                 </div>
             </div>
         </div>
     </details>
 
+    <details class="details">
+        <summary class="summary summary-yellow">Hidden</summary>
+        <div class="map-card map-card-yellow">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HD1</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </div>
+        <div class="map-card map-card-yellow">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HD2</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </div>
+        <div class="map-card map-card-yellow">
+            <div class="map-img" style="background: url('{{asset('images/cover.jpg')}}') no-repeat; background-size: cover">
+                <h2>Phalanx</h2>
+                <h3>USAO</h3>
+            </div>
+            <div class="map-mode">
+                <p>HD3</p>
+            </div>
+            <div class="map-stats">
+                <div class="map-stats-left">
+                    <span><img src="{{asset('images/icon/star.svg')}}"> 7.57</span>
+                    <span><img src="{{asset('images/icon/bpm.svg')}}"> 208</span>
+                    <span><img src="{{asset('images/icon/time.svg')}}"> 4:09</span>
+                </div>
+                <div class="map-stats-right">
+                    <span><img src="{{asset('images/icon/cs.svg')}}"> 4.2</span>
+                    <span><img src="{{asset('images/icon/ar.svg')}}"> 9.8</span>
+                    <span><img src="{{asset('images/icon/od.svg')}}"> 9.6</span>
+                </div>
+            </div>
+        </div>
+    </details>
 @endsection
